55cf5958522e3c6b90e5c43ac8083064bf528f06
Dario Finocchiaro <dariofinocchiaro93+dev@gmail.com>
2021-04-24 22:32:55 +0200

    Âfeat: add exaple

---
+++ example/lib/fades.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Fades {
+  static List<Color> gray = [
+    Colors.black,
+    Color(0xFF393939),
+    Color(0xFF767676),
+    Color(0xFFE5E5E5),
+    Color(0xFFF3F3F3),
+    Color(0xFFFFFFFF)
+  ];
+}
